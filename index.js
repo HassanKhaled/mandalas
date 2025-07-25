5887ef83f8cc26aa12abbf605fb26544aa363cc6
HassanKhaled <hasan.khaled.fcis@gmail.com>
2025-07-25 14:18:50 +0300

    feat(add_images_animated_div): add images to the animted div to get the basic function running
     improve and enahnce the dynamic function of the page

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ let animated = document.getElementById('animated');
  for(let i = 0 ; i < borders_count ; i++)
    borders[i]=`borders_${i+1}.svg`;
 
- for(let i = 0 ; i < mands_count ; i++)
-    mands[i]=`mand_${i+1}.svg`;
+ for(let i = 0 ; i < mands_count ; i++){
+  mands[i]=`mands/mand_${i+1}.svg`;
+  let img = document.createElement('img');
+  img.src=mands[i];
+  animated.appendChild(img);
+
+ }
+    
 
