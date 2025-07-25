ee6e09e96a8a3cb23ef629d481d15d38c5549544
HassanKhaled <hasan.khaled.fcis@gmail.com>
2025-07-25 14:15:58 +0300

    feat(create_animated_div): creeate animated div that will be anchor to add new mandalas
     improve and enahnce teh dynamic function of the page

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
  let mands = [];
  let mands_count = 9;
 
+let animated = document.getElementById('animated');
+
  for(let i = 0 ; i < borders_count ; i++)
    borders[i]=`borders_${i+1}.svg`;
 
  for(let i = 0 ; i < mands_count ; i++)
     mands[i]=`mand_${i+1}.svg`;
+
