95721949743a84f0ea1bc48bd8805a21653f6ba4
HassanKhaled <hasan.khaled.fcis@gmail.com>
2025-07-25 14:28:39 +0300

    fix(remove_first_manadala): remove first mandala as the design without not following the conventional wisedom in the style
     removing it as starting the loop from 1 rather than 0 which is the first mandala

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let animated = document.getElementById('animated');
  for(let i = 0 ; i < borders_count ; i++)
    borders[i]=`borders_${i+1}.svg`;
 
- for(let i = 0 ; i < mands_count ; i++){
+ for(let i = 1 ; i < mands_count ; i++){
   mands[i]=`mands/mand_${i+1}.svg`;
   let img = document.createElement('img');
   img.src=mands[i];
