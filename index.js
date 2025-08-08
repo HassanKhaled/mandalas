cc7dc9a9a768a2996ffaf7db3e9f86359da52504
HassanKhaled <hasan.khaled.fcis@gmail.com>
2025-08-08 19:30:45 +0300

    feat(add_mandala_generation_number): add new mandala generation number to enable generating new mandalas
     improve and enahnce teh dynamic function of the page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  let borders_count = 27;
 
  let mands = [];
- let mands_count = 9;
+ let mands_count = 16;
 
 let animated = document.getElementById('animated');
 let bordered = document.getElementById('bordered');
